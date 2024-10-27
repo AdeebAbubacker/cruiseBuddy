9595e30f10b63524229e3595e3b6cec094f80bfa
Adeeb Abubacker <adeebabu655@gmail.com>
2024-10-27 21:13:25 +0530

    Naviagtion set in splash screen

--- lib/UI/Screens/Splash/splash_screen.dart
+++ lib/UI/Screens/Splash/splash_screen.dart
@@ -1,4 +1,4 @@
-import 'package:cruise_buddy/UI/Screens/Home/home_screen.dart';
+import 'package:cruise_buddy/UI/Screens/On%20Boarding/onboardingscreen_one.dart';
 import 'package:cruise_buddy/core/constants/colors/app_colors.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
@@ -35,14 +35,14 @@ class _SplashScreenState extends State<SplashScreen>
       }
     });
 
-    // Future.delayed(const Duration(milliseconds: 2600), () {
-    //   if (mounted) {
-    //     Navigator.push(
-    //       context,
-    //       MaterialPageRoute(builder: (context) => const HomeScreen()),
-    //     );
-    //   }
-    // });
+    Future.delayed(const Duration(milliseconds: 2600), () {
+      if (mounted) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const OnboardingScreenOne()),
+        );
+      }
+    });
   }
 
   @override
