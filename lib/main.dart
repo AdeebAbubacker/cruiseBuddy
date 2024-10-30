36b81e85d0d9b9fccfc8e44e9115b86289900eb9
Adeeb Abubacker <adeebabu655@gmail.com>
2024-10-30 11:58:58 +0530

    Widgets Flutter Binding Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:cruise_buddy/UI/Screens/Splash/splash_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
